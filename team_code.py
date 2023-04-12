46aea909b3709c1c39c32139b8612c72a742db30
Singstad90 <bjorn_sing@hotmail.com>
2023-04-12 21:20:19 +0200

    add cv code to train and run code

--- team_code.py
+++ team_code.py
@@ -15,11 +15,7 @@ import numpy as np, os, sys
 import random
 import pandas as pd
 from io import StringIO
-import mne
-from sklearn.impute import SimpleImputer
-from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.model_selection import KFold
-import joblib
 import tensorflow as tf
 from scipy import signal
 from typing import Generator
@@ -33,12 +29,19 @@ import coral_ordinal as coral
 
 # Train your model.
 def train_challenge_model(data_folder, model_folder, verbose):
+    
+    SIGNAL_LEN = 30000 # samples
+    BATCH_SIZE = 20
+    EPOCHS = 5
+    LEARNING_RATE = 0.00001
     # Find data files.
     if verbose >= 1:
         print('Finding the Challenge data...')
 
     patient_ids = find_data_folders(data_folder)
+    patient_ids = np.asarray(patient_ids)
     num_patients = len(patient_ids)
+    
 
     if num_patients==0:
         raise FileNotFoundError('No data was provided.')
@@ -46,57 +49,29 @@ def train_challenge_model(data_folder, model_folder, verbose):
     # Create a folder for the model if it does not already exist.
     os.makedirs(model_folder, exist_ok=True)
 
-    # Extract the features and labels.
+    # Get data filenames.
     if verbose >= 1:
-        print('Extracting features and labels from the Challenge data...')
+        print('Get valid filenames...')
 
-    features = list()
-    outcomes = list()
-    cpcs = list()
+    filenames = get_valid_filenames_from_patient_ids(data_folder, patient_ids)
+    random.shuffle(filenames)
 
-    for i in range(num_patients):
-        if verbose >= 2:
-            print('    {}/{}...'.format(i+1, num_patients))
+    # Define the models.
+    if verbose >= 1:
+        print('Building models...')
 
-        # Load data.
-        patient_id = patient_ids[i]
-        patient_metadata, recording_metadata, recording_data = load_challenge_data(data_folder, patient_id)
-
-        # Extract features.
-        current_features = get_features(patient_metadata, recording_metadata, recording_data)
-        features.append(current_features)
-
-        # Extract labels.
-        current_outcome = get_outcome(patient_metadata)
-        outcomes.append(current_outcome)
-        current_cpc = get_cpc(patient_metadata)
-        cpcs.append(current_cpc)
-
-    features = np.vstack(features)
-    outcomes = np.vstack(outcomes)
-    cpcs = np.vstack(cpcs)
+    cpc_model = build_iception_model((SIGNAL_LEN,18), 5)
+    cpc_model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate = LEARNING_RATE), loss = coral.OrdinalCrossEntropy(), metrics = [coral.MeanAbsoluteErrorLabels()])
 
     # Train the models.
     if verbose >= 1:
         print('Training the Challenge models on the Challenge data...')
 
-    # Define parameters for random forest classifier and regressor.
-    n_estimators   = 123  # Number of trees in the forest.
-    max_leaf_nodes = 456  # Maximum number of leaf nodes in each tree.
-    random_state   = 789  # Random state; set for reproducibility.
-
-    # Impute any missing features; use the mean value by default.
-    imputer = SimpleImputer().fit(features)
-
-    # Train the models.
-    features = imputer.transform(features)
-    outcome_model = RandomForestClassifier(
-        n_estimators=n_estimators, max_leaf_nodes=max_leaf_nodes, random_state=random_state).fit(features, outcomes.ravel())
-    cpc_model = RandomForestRegressor(
-        n_estimators=n_estimators, max_leaf_nodes=max_leaf_nodes, random_state=random_state).fit(features, cpcs.ravel())
+    cpc_model.fit(x = batch_generator(batch_size=BATCH_SIZE, signal_len=SIGNAL_LEN, gen = generate_data(data_folder, filenames,samples=SIGNAL_LEN)), 
+                  epochs=EPOCHS, steps_per_epoch=len(filenames)/BATCH_SIZE, verbose=verbose)
 
     # Save the models.
-    save_challenge_model(model_folder, imputer, outcome_model, cpc_model)
+    save_challenge_model(model_folder, cpc_model)
 
     if verbose >= 1:
         print('Done.')
@@ -104,33 +79,48 @@ def train_challenge_model(data_folder, model_folder, verbose):
 # Load your trained models. This function is *required*. You should edit this function to add your code, but do *not* change the
 # arguments of this function.
 def load_challenge_models(model_folder, verbose):
-    filename = os.path.join(model_folder, 'models.sav')
-    return joblib.load(filename)
+    SIGNAL_LEN = 30000 # samples
+    model = build_iception_model((SIGNAL_LEN,18), 5)
+    filename = os.path.join(model_folder, 'model_weights.h5')
+    model.load_weights(filename)
+    if verbose >= 1:
+        print('Model loaded...')
+    return model
+
 
 # Run your trained models. This function is *required*. You should edit this function to add your code, but do *not* change the
 # arguments of this function.
 def run_challenge_models(models, data_folder, patient_id, verbose):
-    imputer = models['imputer']
-    outcome_model = models['outcome_model']
-    cpc_model = models['cpc_model']
+    SIGNAL_LEN = 30000 # samples
 
     # Load data.
     patient_metadata, recording_metadata, recording_data = load_challenge_data(data_folder, patient_id)
 
-    # Extract features.
-    features = get_features(patient_metadata, recording_metadata, recording_data)
-    features = features.reshape(1, -1)
+    patient_pred = [] 
+    for recording in recording_data:
+        if recording[1] == None:
+            patient_pred.append(np.nan)
+        else:
+            current_recording = recording[0]
+            current_recording = np.moveaxis(current_recording,0,-1)
+            current_prediction = models.predict(np.expand_dims(current_recording[:SIGNAL_LEN],0))
+            current_prediction = np.asarray(coral.ordinal_softmax(current_prediction))
+            current_outcome_hat_probability = current_prediction[:,3:].sum(axis=1)
+            patient_pred.append(current_outcome_hat_probability)
+    patient_pred = np.asarray(patient_pred)
 
-    # Impute missing data.
-    features = imputer.transform(features)
+    outcome_probability = np.nanmean(patient_pred)
+    outcome = int((outcome_probability > 0.5) * 1)
 
-    # Apply models to features.
-    outcome = outcome_model.predict(features)[0]
-    outcome_probability = outcome_model.predict_proba(features)[0, 1]
-    cpc = cpc_model.predict(features)[0]
+
+    cpc = (outcome_probability * 4) + 1
+    cpc = np.nan_to_num(cpc, nan=3.0)
 
     # Ensure that the CPC score is between (or equal to) 1 and 5.
     cpc = np.clip(cpc, 1, 5)
+
+    if verbose >= 1:
+        print('Prediction on patient N succeeded...')
 
     return outcome, outcome_probability, cpc
 
@@ -160,10 +150,6 @@ def cross_validate_model(data_folder, num_folds, verbose):
 
     # Create a folder for the model if it does not already exist.
     #os.makedirs(model_folder, exist_ok=True)
-
-    # Extract the features and labels.
-    if verbose >= 1:
-        print('Extracting features and labels from the Challenge data...')
 
     # Make CV folds
     if verbose >= 1:
@@ -203,7 +189,7 @@ def cross_validate_model(data_folder, num_folds, verbose):
         if verbose >= 1:
             print('Training the Challenge models on the Challenge data...')
     
-        cpc_model.fit(x = batch_generator(batch_size=BATCH_SIZE, signal_len=SIGNAL_LEN, gen = generate_data(data_folder, train_filenames)), epochs=EPOCHS, 
+        cpc_model.fit(x = batch_generator(batch_size=BATCH_SIZE, signal_len=SIGNAL_LEN, gen = generate_data(data_folder, train_filenames,samples=SIGNAL_LEN)), epochs=EPOCHS, 
                                               validation_data=batch_generator(batch_size=BATCH_SIZE, signal_len=SIGNAL_LEN, gen = generate_data(data_folder, val_filenames)),
                                               steps_per_epoch=len(train_filenames)/BATCH_SIZE, validation_steps=len(val_filenames)/BATCH_SIZE,validation_freq=1)
 
@@ -260,95 +246,15 @@ def cross_validate_model(data_folder, num_folds, verbose):
     return all_preds_outcome , all_labels_outcome , challenge_score, auroc_outcomes, auprc_outcomes, accuracy_outcomes, f_measure_outcomes, mse_cpcs, mae_cpcs
 
 # Save your trained model.
-def save_challenge_model(model_folder, imputer, outcome_model, cpc_model):
-    d = {'imputer': imputer, 'outcome_model': outcome_model, 'cpc_model': cpc_model}
-    filename = os.path.join(model_folder, 'models.sav')
-    joblib.dump(d, filename, protocol=0)
+#def save_challenge_model(model_folder, imputer, outcome_model, cpc_model):
+#    d = {'imputer': imputer, 'outcome_model': outcome_model, 'cpc_model': cpc_model}
+#    filename = os.path.join(model_folder, 'models.sav')
+#    joblib.dump(d, filename, protocol=0)
 
-# Extract features from the data.
-def get_features(patient_metadata, recording_metadata, recording_data):
-    # Extract features from the patient metadata.
-    age = get_age(patient_metadata)
-    sex = get_sex(patient_metadata)
-    rosc = get_rosc(patient_metadata)
-    ohca = get_ohca(patient_metadata)
-    vfib = get_vfib(patient_metadata)
-    ttm = get_ttm(patient_metadata)
+def save_challenge_model(model_folder, model):
+    filename = os.path.join(model_folder, 'model_weights.h5')
+    model.save_weights(filename)
 
-    # Use one-hot encoding for sex; add more variables
-    sex_features = np.zeros(2, dtype=int)
-    if sex == 'Female':
-        female = 1
-        male   = 0
-        other  = 0
-    elif sex == 'Male':
-        female = 0
-        male   = 1
-        other  = 0
-    else:
-        female = 0
-        male   = 0
-        other  = 1
-
-    # Combine the patient features.
-    patient_features = np.array([age, female, male, other, rosc, ohca, vfib, ttm])
-
-    # Extract features from the recording data and metadata.
-    channels = ['Fp1-F7', 'F7-T3', 'T3-T5', 'T5-O1', 'Fp2-F8', 'F8-T4', 'T4-T6', 'T6-O2', 'Fp1-F3',
-                'F3-C3', 'C3-P3', 'P3-O1', 'Fp2-F4', 'F4-C4', 'C4-P4', 'P4-O2', 'Fz-Cz', 'Cz-Pz']
-    num_channels = len(channels)
-    num_recordings = len(recording_data)
-
-    # Compute mean and standard deviation for each channel for each recording.
-    available_signal_data = list()
-    for i in range(num_recordings):
-        signal_data, sampling_frequency, signal_channels = recording_data[i]
-        if signal_data is not None:
-            signal_data = reorder_recording_channels(signal_data, signal_channels, channels) # Reorder the channels in the signal data, as needed, for consistency across different recordings.
-            available_signal_data.append(signal_data)
-
-    if len(available_signal_data) > 0:
-        available_signal_data = np.hstack(available_signal_data)
-        signal_mean = np.nanmean(available_signal_data, axis=1)
-        signal_std  = np.nanstd(available_signal_data, axis=1)
-    else:
-        signal_mean = float('nan') * np.ones(num_channels)
-        signal_std  = float('nan') * np.ones(num_channels)
-
-    # Compute the power spectral density for the delta, theta, alpha, and beta frequency bands for each channel of the most
-    # recent recording.
-    index = None
-    for i in reversed(range(num_recordings)):
-        signal_data, sampling_frequency, signal_channels = recording_data[i]
-        if signal_data is not None:
-            index = i
-            break
-
-    if index is not None:
-        signal_data, sampling_frequency, signal_channels = recording_data[index]
-        signal_data = reorder_recording_channels(signal_data, signal_channels, channels) # Reorder the channels in the signal data, as needed, for consistency across different recordings.
-
-        delta_psd, _ = mne.time_frequency.psd_array_welch(signal_data, sfreq=sampling_frequency,  fmin=0.5,  fmax=8.0, verbose=False)
-        theta_psd, _ = mne.time_frequency.psd_array_welch(signal_data, sfreq=sampling_frequency,  fmin=4.0,  fmax=8.0, verbose=False)
-        alpha_psd, _ = mne.time_frequency.psd_array_welch(signal_data, sfreq=sampling_frequency,  fmin=8.0, fmax=12.0, verbose=False)
-        beta_psd,  _ = mne.time_frequency.psd_array_welch(signal_data, sfreq=sampling_frequency, fmin=12.0, fmax=30.0, verbose=False)
-
-        delta_psd_mean = np.nanmean(delta_psd, axis=1)
-        theta_psd_mean = np.nanmean(theta_psd, axis=1)
-        alpha_psd_mean = np.nanmean(alpha_psd, axis=1)
-        beta_psd_mean  = np.nanmean(beta_psd,  axis=1)
-
-        quality_score = get_quality_scores(recording_metadata)[index]
-    else:
-        delta_psd_mean = theta_psd_mean = alpha_psd_mean = beta_psd_mean = float('nan') * np.ones(num_channels)
-        quality_score = float('nan')
-
-    recording_features = np.hstack((signal_mean, signal_std, delta_psd_mean, theta_psd_mean, alpha_psd_mean, beta_psd_mean, quality_score))
-
-    # Combine the features from the patient metadata and the recording data and metadata.
-    features = np.hstack((patient_features, recording_features))
-
-    return features
 
 def _inception_module(input_tensor, stride=1, activation='linear', use_bottleneck=True, kernel_size=40, bottleneck_size=32, nb_filters=32):
 
@@ -458,7 +364,7 @@ def batch_generator(batch_size: int, gen: Generator, signal_len:int):
         yield batch_features, batch_labels
 
 
-def generate_data(folder: str, filenames, time=300):
+def generate_data(folder: str, filenames, samples):
     while True:
         for filename in filenames:
             patient_id = get_patient_id_from_path(filename)
@@ -466,7 +372,7 @@ def generate_data(folder: str, filenames, time=300):
             #current_outcome = get_outcome(patient_metadata)
             current_cpc = get_cpc(patient_metadata)
             recording_data, sampling_frequency, _ = load_recording(filename)
-            recording_data = np.moveaxis(recording_data,0,-1)[:int(time*sampling_frequency)]
+            recording_data = np.moveaxis(recording_data,0,-1)[:int(samples)]
             yield recording_data, current_cpc
 
 def map_regression_to_proba(pred):
@@ -485,7 +391,7 @@ def map_proba_to_regression(pred):
         new_pred = 0.5 + (pred-3) / 4
     return new_pred
 
-def get_patient_id_from_path(path,location = "colab"):
+def get_patient_id_from_path(path,location = "local"):
     if location == "colab":
         return path.split("/")[-2]
     elif location == "local":
